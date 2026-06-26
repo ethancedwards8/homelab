5a8641c4927513043a2cd59a791719cfecf8ea01
Ethan Carter Edwards <ethan@ethancedwards.com>
2026-06-26 02:32:19 -0400

    nix flake init
    Signed-off-by: Ethan Carter Edwards <ethan@ethancedwards.com>

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Ethan's Homelab";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs@{ self, nixpkgs, ... }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+    in
+    {
+      devShell = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        with pkgs;
+        mkShell {
+          buildInputs = [
+            git
+            nixfmt
+            opentofu
+          ];
+        }
+      );
+    };
+}
