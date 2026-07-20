508162e977e0ed851e62054ba2d8e57134a899c4
Ethan Carter Edwards <ethan@ethancedwards.com>
2026-07-20 04:19:30 -0400

    tofu state bucket

--- tofu/terraform.tf
+++ tofu/terraform.tf
@@ -11,3 +11,8 @@ terraform {
   }
 }
 
+resource "cloudflare_r2_bucket" "homelab-tf-state-bucket" {
+  account_id = "14a8704b05622c623affefb0d8dd93d4"
+  name = "homelab-tf-state-bucket"
+  location = "ENAM"
+}
