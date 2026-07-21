3842f5acdb84547b9ae0ebc241035053fc5768b4
Ethan Carter Edwards <ethan@ethancedwards.com>
2026-07-21 02:03:48 -0400

    theridgepodcast-media bucket domain

--- tofu/main.tf
+++ tofu/main.tf
@@ -18,18 +18,33 @@ resource "cloudflare_r2_bucket" "tarball-cache-bucket" {
   location = "ENAM"
 }
 
-
+# sftr-media.theridgepodcast.com
 resource "cloudflare_r2_bucket" "sftr-media-bucket" {
   account_id = var.account_id
   name = "sftr-media"
   location = "ENAM"
 }
 
-# sftr-media.theridgepodcast.com
 resource "cloudflare_r2_custom_domain" "sftr-media-domain" {
   account_id = var.account_id
   bucket_name = "sftr-media"
   domain = "sftr-media.theridgepodcast.com"
+  enabled = true
+  zone_id = var.theridgepodcast_zoneid
+  min_tls = "1.0"
+}
+
+# media.theridgepodcast.com
+resource "cloudflare_r2_bucket" "theridgepodcast-media-bucket" {
+  account_id = var.account_id
+  name = "theridgepodcast-media"
+  location = "ENAM"
+}
+
+resource "cloudflare_r2_custom_domain" "theridgepodcast-media-domain" {
+  account_id = var.account_id
+  bucket_name = "theridgepodcast-media"
+  domain = "media.theridgepodcast.com"
   enabled = true
   zone_id = var.theridgepodcast_zoneid
   min_tls = "1.0"
